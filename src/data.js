10c97e44cb3e38d26b5118a08a6399eec9c7955e
sofianenabli <sofienne.nabli@aleia.io>
2022-01-18 12:04:07 +0100

    slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://casdbeavertales.org/wp-content/uploads/2020/12/pexels-photo-773594.jpeg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://casdbeavertales.org/wp-content/uploads/2020/12/pexels-photo-773594.jpeg",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://casdbeavertales.org/wp-content/uploads/2020/12/pexels-photo-773594.jpeg",
+        title: "AUTUMN SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fbf0f4",
+    }
+]
