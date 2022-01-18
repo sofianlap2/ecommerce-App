d19e79cfc54bc9c7e0cfaa39cf3f14f8aa020fd0
sofianenabli <sofienne.nabli@aleia.io>
2022-01-18 16:36:01 +0100

    Register

--- src/data.js
+++ src/data.js
@@ -1,23 +1,76 @@
 export const sliderItems = [
     {
         id: 1,
-        img: "https://casdbeavertales.org/wp-content/uploads/2020/12/pexels-photo-773594.jpeg",
+        img: "https://5.imimg.com/data5/SELLER/Default/2021/12/UF/ER/OT/45025556/men-printed-shirt-500x500.jpg",
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
         bg: "f5fafd",
     },
     {
         id: 2,
-        img: "https://casdbeavertales.org/wp-content/uploads/2020/12/pexels-photo-773594.jpeg",
+        img: "https://5.imimg.com/data5/TY/IB/PV/SELLER-82112303/mens-tommy-hilfiger-t-shirt-500x500.jpg",
         title: "WINTER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
         bg: "fcf1ed",
     },
     {
         id: 3,
-        img: "https://casdbeavertales.org/wp-content/uploads/2020/12/pexels-photo-773594.jpeg",
+        img: "https://botero.com.au/wp-content/uploads/sandy_with_logo_02.jpg",
         title: "AUTUMN SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
         bg: "fbf0f4",
     }
 ]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://botero.com.au/wp-content/uploads/mens-tshirt-front-1.jpg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://botero.com.au/wp-content/uploads/ladies-tshirt-front-3.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://botero.com.au/wp-content/uploads/bird-1-600x600.jpg",
+        title: "LIGHT JACKETS",
+    }
+]
+
+export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
---
+++ src/pages/Register.jsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background: url("https://images2.alphacoders.com/133/thumb-1920-133352.jpg") center no-repeat;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Wrapper = styled.div`
+    padding: 20px;
+    width: 40%;
+    background-color: white;
+`;
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0 0;
+    padding: 10px;
+`;
+
+const Agreement = styled.span`
+    font-size: 12px;
+    margin: 20px 0;
+`;
+
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
+
+const Register = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>Create an account</Title>
+                <Form>
+                    <Input placeholder="name" />
+                    <Input placeholder="last name" />
+                    <Input placeholder="username" />
+                    <Input placeholder="email" />
+                    <Input placeholder="password" />
+                    <Input placeholder="confirm password" />
+                    <Agreement>
+                        By creating an account , 
+                        I consent to the processing of my personal data 
+                        in accourdance with the <b>PRIVACY POLICY</b>
+                    </Agreement>
+                    <Button>CREATE</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Register
